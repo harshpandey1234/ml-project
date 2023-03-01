bef7af111820b7718c3967e2290b6d12d2fe051c
HARSH PANDEY <78255950+harshpandey1234@users.noreply.github.com>
2023-03-01 16:35:44 +0530

    Update final_app_2.py

--- final_app_2.py
+++ final_app_2.py
@@ -97,7 +97,7 @@ def main():
 
   input_features = pd.DataFrame.from_dict(input_features)
   
-  if st.button('Prdict Marks'):
+  if st.button('Predict Marks'):
     pred_marks = predict_g3(input_features)
     st.success(f'The predicted mark is: {pred_marks}')
 
@@ -114,7 +114,7 @@ def add_bg_from_url():
          <style>
          .stApp {{
           
-             background-image: url("https://images.unsplash.com/photo-1510070112810-d4e9a46d9e91?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2069&q=80");
+             background-image: url("https://images.unsplash.com/photo-1497864149936-d3163f0c0f4b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80");
              background-attachment: fixed;
              background-size: cover
          }}
